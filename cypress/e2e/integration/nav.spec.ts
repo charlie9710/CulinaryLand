cd61b73fa7d7a8f3afc5ec5cbe9b333617024f87
charlie9710 <carfe08@hotmail.com>
2024-07-12 16:46:33 -0400

    ETF Final

--- cypress/e2e/integration/nav.spec.ts
+++ cypress/e2e/integration/nav.spec.ts
@@ -1,10 +1,10 @@
 describe('Navigation Tests', () => {
-    it('should navigate to the about page', () => {
+    it('should navigate to the about page without SVG icons', () => {
         cy.on('uncaught:exception', (err, runnable) => {
-            console.log('Error:', err);
-            return false;
+            return false; // Retorna false para evitar que el error detenga la prueba
         });
-        
         cy.visit('http://localhost:8100');
+        cy.wait(5000); // Ajusta el tiempo de espera según sea necesario
+
     });
 });
